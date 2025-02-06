a8e85b0b21d41ef32be746aa05a9cebac8d825a0
zhiqianglucky <41577084+zhiqianglucky@users.noreply.github.com>
2025-02-06 17:53:27 -0500

    Update profile.py

--- profile.py
+++ profile.py
@@ -1,7 +1,7 @@
-"""An example of constructing a profile with two VMs connected by a LAN.
+"""An example of constructing a profile with two ARM64 nodes connected by a LAN.
 
 Instructions:
-Wait for the profile instance to start, and then log in to either VM via the
+Wait for the profile instance to start, and then log in to either host via the
 ssh ports specified below.
 """
 
@@ -10,11 +10,15 @@ import geni.rspec.pg as rspec
 
 request = portal.context.makeRequestRSpec()
 
-# Create two XenVM nodes.
-node1 = request.XenVM("node1")
-node2 = request.XenVM("node2")
+# Create two raw "PC" nodes
+node1 = request.RawPC("node1")
+node2 = request.RawPC("node2")
+
+# Set each of the two to specifically request "m400" nodes, which in CloudLab, are ARM
+node1.hardware_type = "m400"
+node2.hardware_type = "m400"
 
 # Create a link between them
-link1 = request.Link(members = [node1,node2])
+link1 = request.Link(members = [node1, node2])
 
 portal.context.printRequestRSpec()
